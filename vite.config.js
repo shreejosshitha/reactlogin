cfd5277f91def623e2056538f99b624630d68b75
shreejosshitha <shreejosshitha@gmail.com>
2025-02-11 21:23:29 +0530

    Updated navbar styles

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/reactlogin/",
   plugins: [react()],
-})
+  base: "/reactlogin",
+  build: {
+    rollupOptions: {
+      input: "src/main.jsx",
+    },
+  },
+});
